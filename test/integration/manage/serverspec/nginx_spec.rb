d7d58dbfbd161242d1fb7646cba2c705b3fc277b
Adham Helal <aa@hellofresh.com>
2016-03-03 11:23:38 +0100

    custom content

--- test/integration/manage/serverspec/nginx_spec.rb
+++ test/integration/manage/serverspec/nginx_spec.rb
@@ -48,20 +48,14 @@ end
 
 describe 'NGinx http content' do
 
- describe command "curl -s -L http://127.0.0.1:1180 | grep h1" do 
+ describe command "curl -s -L http://127.0.0.1:1180" do 
     its(:exit_status) { should eq 0 }
-    its(:stdout) { should match "<h1>Welcome to nginx!</h1>" }
+    its(:stdout) { should match "Hello test2" }
   end
 
- describe command "curl -s -L http://127.0.0.1:1280 | grep h1" do 
+ describe command "curl -s -L http://127.0.0.1:1280" do 
     its(:exit_status) { should eq 0 }
-    its(:stdout) { should match "<h1>Welcome to nginx!</h1>" }
-  end
-
- describe command "cat /var/log/nginx/*" do 
-    its(:exit_status) { should eq 0 }
-    its(:stdout) { should match "XXXXXXXX" }
+    its(:stdout) { should match "Hello test3" }
   end
 
 end
-
